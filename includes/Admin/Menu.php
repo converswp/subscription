bfa306535b70543c53c7be2fa1c83f59404c14a8
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2025-05-08 18:24:13 +0600

    added logo to menu

--- includes/Admin/Menu.php
+++ includes/Admin/Menu.php
@@ -21,6 +21,14 @@ class Menu {
      */
     public function create_admin_menu() {
         $slug = 'edit.php?post_type=subscrpt_order';
-        add_menu_page( __( 'Subscriptions', 'sdevs_subscrpt' ), __( 'Subscriptions', 'sdevs_subscrpt' ), 'manage_options', $slug, false, 'dashicons-image-rotate', 40 );
+        add_menu_page( 
+            __( 'Subscriptions', 'sdevs_subscrpt' ), 
+            __( 'Subscriptions', 'sdevs_subscrpt' ), 
+            'manage_options', 
+            $slug, 
+            false, 
+            SUBSCRPT_ASSETS . '/images/icons/subscription-20-gray.png', 
+            40 
+        );
     }
 }
