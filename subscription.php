1780a7609a83d18c722734ba8bb4067565051532
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2025-05-13 19:32:38 +0600

    released 1.4.0

--- subscription.php
+++ subscription.php
@@ -8,7 +8,7 @@ Plugin URI: https://wpsubscription.co/
 Author: converswp
 Author URI: https://wpsubscription.co/
 
-Version: 1.3.2
+Version: 1.4.0
 License: GPLv2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 Text Domain: wp_subscription
