70ffb9cff518ca56d63b389cb6a296ebe4ea475e
aushamim <au.shamim0@gmail.com>
2025-06-02 15:09:33 +0600

    fix: 🐛 namespace update

--- subscription.php
+++ subscription.php
@@ -171,29 +171,39 @@ final class Sdevs_Subscription {
 		add_action( 'init', array( $this, 'init_classes' ) );
 		add_action( 'init', array( $this, 'localization_setup' ) );
 		add_action( 'init', array( $this, 'run_update' ) );
-		
+
 		// HPOS Compatibility: Declare support for custom order tables
-		add_action('before_woocommerce_init', function() {
-			if (class_exists(\Automattic\WooCommerce\Utilities\FeaturesUtil::class)) {
-				\Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility('custom_order_tables', __FILE__, true);
+		add_action(
+			'before_woocommerce_init',
+			function () {
+				if ( class_exists( \Automattic\WooCommerce\Utilities\FeaturesUtil::class ) ) {
+					\Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility( 'custom_order_tables', __FILE__, true );
+				}
 			}
-		});
+		);
 
 		// HPOS Compatibility: Register subscription post type with HPOS support
-		add_action('init', function() {
-			register_post_type('subscrpt_order', array(
-				'hpos' => true,
-				'public' => false,
-				'show_ui' => true,
-				'show_in_menu' => false,
-				'supports' => array('title'),
-				'capability_type' => 'post',
-				'capabilities' => array(
-					'create_posts' => false,
-				),
-				'map_meta_cap' => true,
-			));
-		}, 0);
+		add_action(
+			'init',
+			function () {
+				register_post_type(
+					'subscrpt_order',
+					array(
+						'hpos'            => true,
+						'public'          => false,
+						'show_ui'         => true,
+						'show_in_menu'    => false,
+						'supports'        => array( 'title' ),
+						'capability_type' => 'post',
+						'capabilities'    => array(
+							'create_posts' => false,
+						),
+						'map_meta_cap'    => true,
+					)
+				);
+			},
+			0
+		);
 	}
 
 	/**
@@ -216,7 +226,7 @@ final class Sdevs_Subscription {
 			$this->container['ajax'] = new SpringDevs\Subscription\Ajax();
 		}
 
-		$this->container['api']    = new SpringDevs\Subscription\Api();
+		$this->container['api']    = new SpringDevs\Subscription\API();
 		$this->container['assets'] = new SpringDevs\Subscription\Assets();
 	}
 
