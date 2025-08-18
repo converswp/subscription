31224fc3a314f4e22d82c35ebec31ccafb966ef8
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2025-08-18 16:54:41 +0600

    script build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-checkout', 'wc-blocks-components', 'wc-price-format', 'wp-i18n', 'wp-plugins'), 'version' => 'bce736c681c299171108');
+<?php return array('dependencies' => array('react', 'wc-blocks-checkout', 'wc-blocks-components', 'wc-price-format', 'wp-i18n', 'wp-plugins'), 'version' => 'c310cd985954dd16d0ca');
